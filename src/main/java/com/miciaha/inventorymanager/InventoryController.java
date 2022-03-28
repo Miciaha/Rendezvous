f0b2ddf5573e706c90227cfe35fed0feeb7852fb
miciaha <mivey37@wgu.edu>
2022-03-28 16:20:28 -0400

    Create main form based on GUI mock-up.

--- src/main/java/com/miciaha/inventorymanager/InventoryController.java
+++ src/main/java/com/miciaha/inventorymanager/InventoryController.java
@@ -1,9 +1,22 @@
 package com.miciaha.inventorymanager;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class InventoryController {
+    public TableView partsTable;
+    public TableView productsTable;
+    public Button btnAddPart;
+    public Button btnModifyPart;
+    public Button btnDeletePart;
+    public Button btnAddProduct;
+    public Button btnModifyProduct;
+    public Button btnDeleteProduct;
+    public TextField tfSearchParts;
+    public TextField tfSearchProducts;
     @FXML
     private Label welcomeText;
 
